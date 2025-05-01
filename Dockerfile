d464576361fc0250824f3e35d6a3fb237c93aa6b
Savanna Bergen <savanna.bergen@gmail.com>
2025-04-30 20:22:31 -0500

    updated for mobile view

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,15 @@
-# Use an official Node.js runtime as the base image
 FROM node:23-slim
 
-# Set the working directory in the container
 WORKDIR /final_site
 
-# Copy the package*.json files to the working directory
 COPY package*.json ./
 
-# Install the dependencies
 RUN npm install
 
-# Copy the application code to the working directory
 COPY . .
 
-# Expose the port the application will run on
 EXPOSE 5575
 
-# Run the command to start the development server
-CMD ["npm", "run", "dev"]
+ENV NODE_ENV=production
+
+CMD ["npm", "start"]
--- src/components/WelcomeMessage.tsx
+++ src/components/WelcomeMessage.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const WelcomeMessageContainer = styled.div`
+  text-align: center;
+  padding: 0px 20px 20px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  margin-top: -20px;
+
+  @media (max-width: 768px) {
+    margin-top: -40px; /* adjust margin for mobile view */
+  }
+`;
 
 const WelcomeMessage = () => {
   return (
-    <div style={{
-      textAlign: 'center',
-      padding: '20px',
-      fontSize: '24px',
-      fontWeight: 'bold',
-      color: '#333'
-    }}>
+    <WelcomeMessageContainer>
       <h1>Welcome to My Portfolio</h1>
       <p>I'm Savanna, a Web Developer with a passion for creating innovative solutions.</p>
-    </div>
+    </WelcomeMessageContainer>
   );
 };
-
-export default WelcomeMessage;
